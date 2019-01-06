fe67067fdde220420a5a138fb315700630534e44
Roxanne Reyes <34749680+waetech@users.noreply.github.com>
2019-01-05 22:49:17 -0500

    Update authReducer.js

--- authReducer.js
+++ authReducer.js
@@ -1,14 +1,15 @@
 
 const INITIAL_STATE = {
-    isSignedIn: null
+    isSignedIn: null,
+    userId: null
 };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
-         return {...state, isSignedIn: true};
-         case 'SIGN_OUT':
-           return {...state, isSignedOut: false};
+        case SIGN_OUT:
+         return {...state, isSignedIn: true, userId: action.payload };
+         case SIGN_OUT:
+           return {...state, isSignedOut: false, userId: null };
            default:
            return state;
     }
